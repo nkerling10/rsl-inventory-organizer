20f1d4895e9f27a29686ae30df3594d284fa01e5
nkerling10 <nkerling10@gmail.com>
2020-05-05 00:27:33 -0500

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { ChampionsComponent } from './champions/champions.component';
 import { ItemsComponent } from './items/items.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, ChampionsComponent, ItemsComponent ],
+  declarations: [ AppComponent, ChampionsComponent, ItemsComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
