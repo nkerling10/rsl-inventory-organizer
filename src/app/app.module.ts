7fb18ab42a69258356b0ac1f568c5441ff251065
nkerling10 <nkerling10@gmail.com>
2020-05-05 00:16:43 -0500

    1 binaries uploaded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { ChampionsComponent } from './champions/champions.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, ChampionsComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
